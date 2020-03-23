d0c22c2b852c96f66d0e8b0f0c20f3a8cdeb4df9
victor-grajski <victor.grajski@gmail.com>
2020-03-23 00:22:26 -0700

    update site description

--- siteConfig.js
+++ siteConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: 'Victor Grajski',
   shortName: 'Victor Grajski',
-  description: "I'm a Product Designer, a Master's student at UC Berkeley, and a musician. At the risk of sounding cliché, I aim to design products that put people first.",
+  description: "I'm a Product Designer, a Master's student at UC Berkeley, and a musician. At the risk of sounding cliché, I aim to design products that humanize technology.",
   url: 'https://victor-grajski.github.io', // Domain of your site without prefix!
   prefix: '/',
   author: 'Victor Grajski',
