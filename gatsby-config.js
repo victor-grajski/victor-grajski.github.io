75d197de849334c524d74a0cb97874ea6875fcd4
Victor Grajski <victor.grajski@braingu.com>
2022-03-28 18:06:42 -0700

    whitelist full-width

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,9 +79,9 @@ module.exports = {
       resolve: `gatsby-plugin-purgecss`,
       options: {
         printRejected: true, // Print removed selectors and processed file names
+        whitelist: ["full-width"], // Don't remove this selector
         // develop: true, // Enable while using `gatsby develop`
         // tailwind: true, // Enable tailwindcss support
-        // whitelist: ['whitelist'], // Don't remove this selector
         // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
       },
