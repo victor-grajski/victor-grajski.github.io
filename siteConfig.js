b122708bb895bdd976377975446e589f2e992682
victor-grajski <victor.grajski@gmail.com>
2022-12-26 09:27:05 -0800

    update headline

--- siteConfig.js
+++ siteConfig.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "Victor Grajski",
   shortName: "Victor Grajski",
   description:
-    "I'm a Design Engineer and a musician. I love making shoegazey alternative rock and using code as a creative material to explore the right thing to build.",
+    "I'm a Design Engineer and a musician. I love making shoegazey alternative rock and using code as a creative material to reveal novel and useful insights about how people work in order to converge on the right thing to build.",
   url: "https://victor-grajski.github.io", // Domain of your site without prefix!
   prefix: "/",
   author: "Victor Grajski",
